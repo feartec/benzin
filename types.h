c7aa8eda554ce10a7e3969dd1b11a4501116ff0e
comex <comexk@gmail.com>
2009-01-27 18:32:04 -0500

    fix types.h

--- types.h
+++ types.h
@@ -3,11 +3,11 @@
 
 typedef unsigned char u8;
 typedef unsigned short u16;
-typedef unsigned long u32;
+typedef unsigned int u32;
 typedef unsigned long long u64;
 typedef signed char s8;
 typedef signed short s16;
-typedef signed long s32;
+typedef signed int s32;
 typedef signed long long s64;
 
 typedef float f32;
